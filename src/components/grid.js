c483e0fbca3fb66e6b74ab226efb7c7cca366e22
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2022-01-22 14:24:21 -0500

    removed context from grid

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,33 +1,15 @@
-import React, { useEffect, useContext } from "react";
+import React from "react";
 
 import Node from "./node";
-import ContextObject from "../context/ContextObject";
 
 const Grid = () => {
-	const ctx = useContext(ContextObject);
-
-	useEffect(() => {
-		ctx.getWord();
-		// eslint-disable-next-line
-	}, []);
-
-	useEffect(() => {
-		if (ctx.win) {
-			alert('You have completed the challenge!');
-		} else {
-			if (ctx.attempt >= 6) {
-				alert('Sorry! Try again next time!');
-			}
-		}
-	}, [ctx]);
-
 	const grid = [0, 1, 2, 3, 4, 5];
 	const row = [0, 1, 2, 3, 4];
 
 	return (
-		<div style={{ display: 'flex', flexDirection: 'column' }}>
+		<div className="d-flex flex-column">
 			{grid.map((rowNum) => (
-                <div key={rowNum} id={rowNum} style={{ display: "flex", justifyContent: "center" }}>
+                <div key={rowNum} id={rowNum} className="d-flex justify-content-center">
 					{row.map((colNum) => (
 						<Node id={rowNum * 10 + colNum} />
 					))}
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,8 +6,10 @@ const Home = () => {
 		<div className="App">
 			<h2>Wordly</h2>
 			<h4>Welcome to Wordly</h4>
-            <p><small>This is totally not a ripoff of Wordle</small></p>
-			<div className="d-flex flex-column text-align-center">
+            <p><small>A word-based game to test your vocabulary.</small></p>
+            <p><small>Enter 5 letter words and receive hints.</small></p>
+            <p><small>Use the hints to get closer to the correct answer.</small></p>
+			<div className="d-flex flex-column align-items-center">
 				<strong>Now choose between single and multiplayer!</strong>
 				<Link className="btn btn-light" to="/single">Single</Link>
 				<Link className="btn btn-light" to="/multi">Multiplayer</Link>
