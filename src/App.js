484e25319c8e3555856668e0f967e141d888c62b
Jorge Ribeiro <ribeiroj@purdue.edu>
2022-01-22 01:41:01 -0500

    new input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react'
+import $ from 'jquery';
 
 function App() {
   let result = []
   let solution = "hello"
-  let guess = "cools"
+  let guess = "nices"
+/*   let guess = window.prompt("Enter your guess")
+  while (guess.length < 5) {
+    alert("Not enough letters")
+    guess = window.prompt("Enter your guess")
+  } */
 
-  //let grid = Array(6).fill().map(()=>Array(5).fill())
+  document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.ctrlKey && evt.keyCode == 8) {
+      $(this).prev('.inputs').focus();
+    }
+};
 
-  //grid.forEach(grid.at())
+  $(function() {
+    $(".inputs").keyup(function () {
+        if (this.value.length == this.maxLength) {
+          $(this).next('.inputs').focus();
+        }
+    });
+});
 
-  //console.log(grid)
 
+  
+  
+  // If word not on database, 
+  // then say that it is not a valid word and re-ask
+
+
+  function addToGrid(guess) {
+    let grid = [[0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]]
+
+    let index = 0;
+    let row_index = 0;
+    for (let i = 0; i < guess[row_index].length; i++) {
+      for (let j = 0; j < guess.length; j++) {
+        grid[i][j] = guess.charAt(index)
+        index++
+      }
+    }
+    row_index++
+    console.log(grid)
+  }
   function letterCheck(guess, solution) {
     for (let i = 0; i < guess.length; i++) {
       let guessedLetter = guess.charAt(i);
@@ -28,9 +70,17 @@ function App() {
   }
 
   letterCheck(solution, guess);
+  addToGrid(guess)
   return (
     <div className="App">
+      <input class="inputs" type="text" maxlength="1" />
+      <input class="inputs" type="text" maxlength="1" />
+      <input class="inputs" type="text" maxlength="1" />
+      <input class="inputs" type="text" maxlength="1" />
+      <input class="inputs" type="text" maxlength="1" />
+
     </div>
+
   );
 
 }
